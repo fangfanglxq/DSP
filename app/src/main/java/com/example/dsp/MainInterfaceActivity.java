b51151327fa51715ba3b07205beeab0309c0c452
luoxuqing· <2545774420@qq.com>
2021-06-15 19:55:33 +0800

    main_page

--- app/src/main/java/com/example/dsp/MainInterfaceActivity.java
+++ app/src/main/java/com/example/dsp/MainInterfaceActivity.java
@@ -1,14 +1,14 @@
-package com.example.dsp;
+ package com.example.dsp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainInterfaceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.Main_interface);
     }
 }
